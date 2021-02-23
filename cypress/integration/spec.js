ce35c35e3039cfe487c8ad34936a85c567e8377a
beemi <beemi.raja@gmail.com>
2021-02-23 22:55:49 +0000

    added sample file

---
+++ cypress/integration/spec.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+it.skip('finds element by test id data attribute', () => {
+    cy.visit('index.html')
+    cy.get('[data-test-id="foo"]').should('have.text', 'foo')
+    cy.getDataTestId('foo').should('have.text', 'foo')
+    cy.getDataTest('bar').should('have.text', 'bar')
+    cy.getTestId('baz').should('have.text', 'baz')
+})
